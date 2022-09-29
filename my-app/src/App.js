1fbbbcbde899768849d8b154e8d6cdceb397d18f
Fathan_alfarazi <sansfathan@gmail.com>
2022-09-29 14:52:58 +0700

    master

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,11 @@
 
-
 import React from "react";
 
 function App () {
   return (
     <React.Fragment>
-      <h1 className="bg-red-500 italic text-center">Loading...</h1>
-      
+      <h1 className="bg-red-500">Latihan Export Import</h1>
+     
     </React.Fragment>
   );
 }
