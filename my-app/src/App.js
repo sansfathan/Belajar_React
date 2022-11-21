c0a84804c2099702e1040fba729f7b0763e89f1c
Fathan_alfarazi <sansfathan@gmail.com>
2022-11-21 13:25:57 +0700

    hook-materi

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,11 @@
 
 import React from "react";
-
+import RefTutor from "./page/home";
 function App () {
   return (
     <React.Fragment>
       <h1 className="bg-red-500">Latihan Export Import</h1>
-     
+     <RefTutor/>
     </React.Fragment>
   );
 }
